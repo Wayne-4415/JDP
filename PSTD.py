1da7930b167da433065e38a755d06946a6710bfd
BakeSausage <joseph369351@gmail.com>
2023-06-30 14:21:26 +0800

    20230630_1421

--- PSTD.py
+++ PSTD.py
@@ -4,19 +4,27 @@ import cmath
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib import cm
-from os import _exit
 import time 
 start_time = time.time()
 
-N_x = 120
-N_y = 120
-N_z = 120
-L_x = 40E-6
-L_y = 40E-6
-L_z = 40E-6
-delta_t = 0.05E-15
-permittivity = 8.85E-12
-permeability = 1.25E-6
+
+N_x = 40
+N_y = 40
+N_z = 40
+L_x = 2E-6
+L_y = 2E-6
+L_z = 2E-6
+delta_t = 0.004E-15
+
+permittivity = np.zeros((N_x,N_y,N_z))
+permittivity[:,:,:] = 8.85E-12
+
+permeability = np.zeros((N_x,N_y,N_z))
+permeability[:,:,:] = 1.25E-6
+
+# permittivity[12:15,15:25,15:25] = 12 * 8.85E-12
+# permeability[12:15,15:25,15:25] = 1 * 1.25E-6
+
 pml = np.zeros((N_x, N_y, N_z))
 
 x = np.zeros((N_x))
@@ -46,25 +54,57 @@ def plot(figure,title):
     ax.set_title(title)
     contourf = ax.contourf(np.absolute(figure), 400, vmin=figure.min(), vmax=figure.max(), levels = 50)
     cbar = fig.colorbar(contourf)
+    # ax.plot([15,25,25,15,15],[15,15,12,12,15], color = 'red')
     plt.show()
 
-def plot3d(figure,title):
-    mpl.rcParams['figure.dpi'] = 160
-    mpl.rcParams['figure.figsize'] = [8, 8]  #[x,y] 
-    mpl.rcParams['font.size'] = 13
+def plot3dscatter(figure,title):
+    figure = figure/np.max(figure)
     fig = plt.figure()
-    ax = fig.add_subplot(1, 2, 1, projection='3d')
-    ax.set_title(title)
-    X = np.arange(0, np.shape(figure)[0], 1)
-    Y = np.arange(0, np.shape(figure)[1], 1)
-    X, Y = np.meshgrid(X, Y)
-    Z = figure
-    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=True)
-    ax.set_zlim(-0.5, 1)
+    ax = fig.add_subplot(projection='3d')
+    ax.set_xlim(-L_x/2,L_x/2)
+    ax.set_ylim(-L_y/2,L_y/2)
+    ax.set_zlim(-L_z/2,L_z/2)
+    xxr, yyr, zzr, cr = xx.ravel(), yy.ravel() , zz.ravel(), figure.ravel()
+    ax.scatter(xxr, yyr, zzr, c=cr, s=cr*3, cmap="Reds")
+    plt.axis("off")
     plt.show()
+
+
+
+# def enclosure(x,y,z,x1,y1,z1,x2,y2,z2):
+#     if ((x>=x1) and (x<=x2)) and ((y>=y1) and (y<=y2)) and ((z>=z1) and (z<=z2)):
+#         return 1
+#     else:
+#         return 0
+    
+# def t_f(x,y,z):
+#     # return np.where(np.sqrt(np.square(x) + np.square(y)) < 0.6E-6, 1, 0)
+#     mask = np.zeros((len(x),len(y),len(z)))
+#     for i in range(len(x)):
+#         for j in range(len(x)):
+#             for k in range(len(z)):
+#                 positionX = x[i,0,0]
+#                 positionY = y[0,j,0]
+#                 positionZ = z[0,0,k]
+#                 mask[i,j,k] += enclosure(positionX,positionY,positionZ,-1E-6,0.3E-6,0.2E-6,-0.3E-6,1E-6,0.6E-6)
+#                 mask[i,j,k] += enclosure(positionX,positionY,positionZ,-0.3E-6,0.7E-6,0.2E-6,1E-6,1E-6,0.6E-6)
+#                 mask[i,j,k] += enclosure(positionX,positionY,positionZ,-1E-6,-1E-6,0.2E-6,-0.7E-6,0.3E-6,0.6E-6)
+#                 mask[i,j,k] += enclosure(positionX,positionY,positionZ,-0.7E-6,-1E-6,0.2E-6,1E-6,-0.7E-6,0.6E-6)
+#                 mask[i,j,k] += enclosure(positionX,positionY,positionZ,0.7E-6,-0.7E-6,0.2E-6,1E-6,0.7E-6,0.6E-6)
+#                 mask[i,j,k] += enclosure(positionX,positionY,positionZ,-0.7E-6,-0.1E-6,0.2E-6,0.1E-6,0.1E-6,0.6E-6)
+#                 mask[i,j,k] += enclosure(positionX,positionY,positionZ,-0.1E-6,0.1E-6,0.2E-6,0.1E-6,0.7E-6,0.6E-6)
+#                 mask[i,j,k] += enclosure(positionX,positionY,positionZ,-0.7E-6,-0.5E-6,0.2E-6,0.5E-6,-0.3E-6,0.6E-6)
+#                 mask[i,j,k] += enclosure(positionX,positionY,positionZ,0.3E-6,-0.3E-6,0.2E-6,0.5E-6,0.7E-6,0.6E-6)
+#     return np.where(mask !=0, 12 * 8.85E-12, 8.85E-12)
+
+
+# permittivity = t_f(xx, yy, zz)
+
+# plot3dscatter(permittivity,"p")
+
 
 def PML(r):
-    pml_thickness = 4E-6
+    pml_thickness = 0.1E-6
     Q = 1 / delta_t
     if r < -L_x/2 + pml_thickness:
         return Q * ((-L_x/2 + pml_thickness - r)/pml_thickness)**3
@@ -77,9 +117,7 @@ for i in range(N_x):
     for j in range(N_y):
         for k in range(N_z):
             pml[i,j,k] = PML(x[i]) + PML(y[j]) + PML(z[k])
-# DD=(1 - pml * delta_t / 2)/(1 + pml * delta_t / 2)
-# plot(np.real(DD[:,:,N_z//2]),"vdsg")
-# os.stop
+
 
 
 Hx = np.zeros((N_x,N_y,N_z), dtype = complex)
@@ -94,13 +132,36 @@ kx = 2*pi/L_x * index_xx[:,:,:]
 ky = 2*pi/L_y * index_yy[:,:,:]
 kz = 2*pi/L_z * index_zz[:,:,:]
 
-def Ez_f(x,y,z):
-    return np.exp(-((np.square(x)+np.square(y))/(2E-6)**2))
-    # return np.where(np.sqrt(np.square(x)+np.square(y)+np.square(z))<5E-6,1,0)
 
+def Ex_f(x,y,z):
+    # return np.exp(-((np.square(y)+np.square(z))/(0.2E-6)**2))
+    # temp = np.zeros_like(x)
+    # temp[:,:,20] = 1
+    # return temp
+    return np.divide(np.ones_like(xx),(np.square(xx)+np.square(yy)+np.square(zz)), out=np.ones_like(xx), where=(np.square(xx)+np.square(yy)+np.square(zz))!=0)
+
+def Ey_f(x,y,z):
+    # return np.exp(-((np.square(x)+np.square(z))/(0.2E-6)**2))
+    # temp = np.zeros_like(x)
+    # temp[:,:,20] = 1
+    # return temp
+    return np.divide(np.ones_like(xx),(np.square(xx)+np.square(yy)+np.square(zz)), out=np.ones_like(xx), where=(np.square(xx)+np.square(yy)+np.square(zz))!=0)
+
+def Ez_f(x,y,z):
+    # return np.exp(-((np.square(x)+np.square(y))/(0.2E-6)**2))
+    # return np.zeros_like(xx)
+    return np.divide(np.ones_like(xx),(np.square(xx)+np.square(yy)+np.square(zz)), out=np.ones_like(xx), where=(np.square(xx)+np.square(yy)+np.square(zz))!=0)
+    
 
 Ez = Ez_f(xx,yy,zz)
-# plot(np.absolute(Ez[:,:,N//2]),"Ez")
+Ex = Ex_f(xx,yy,zz)
+Ey = Ey_f(xx,yy,zz)
+
+Ex0 = Ex_f(xx,yy,zz)
+Ey0 = Ey_f(xx,yy,zz)
+Ez0 = Ez_f(xx,yy,zz)
+
+plot3dscatter(np.absolute(np.sqrt(np.square(Ex)+np.square(Ey)+np.square(Ez))),"initial E")
 
 def fold(M):
     folded_M = np.zeros((N_x,N_y,N_z), dtype = complex)
@@ -114,7 +175,9 @@ def fold(M):
     folded_M[N_x//2:N_x, N_y//2:N_y, N_z//2:N_z] = M[0:N_x//2, 0:N_y//2, 0:N_z//2]
     return folded_M
 
-for t in range(2101):
+E = np.zeros((N_x,N_y,N_z), dtype = complex)
+
+for t in range(1201):
     fft_y_Ex = np.fft.fft(Ex, axis = 1)
     fft_z_Ex = np.fft.fft(Ex, axis = 2)
     fft_x_Ey = np.fft.fft(Ey, axis = 0)
@@ -194,10 +257,15 @@ for t in range(2101):
     Ey = (1 - pml * delta_t / 2)/(1 + pml * delta_t / 2) * Ey + (delta_t/permittivity) * (1 / (1 + pml * delta_t / 2)) * (ifft_kz_fft_z_Hx - ifft_kx_fft_x_Hz)
     Ez = (1 - pml * delta_t / 2)/(1 + pml * delta_t / 2) * Ez + (delta_t/permittivity) * (1 / (1 + pml * delta_t / 2)) * (ifft_kx_fft_x_Hy - ifft_ky_fft_y_Hx)
     
+    # Ex += Ex0
+    # Ey += Ey0
+    # Ez += Ez0
+    
     print(t)
-    if t%300 == 0:
-        plot3d(np.real(Ez[:,:,N_z//2]),t)
+    if t%100 == 0:
+        # plot3d(np.real(Ez[:,:,N_z//2]),t)
+        # plot(np.absolute(np.sqrt(np.square(Ex)+np.square(Ey)+np.square(Ez)))[31,:,:],t)
+        plot3dscatter(np.absolute(np.sqrt(np.square(Ex)+np.square(Ey)+np.square(Ez))),t)
 # plot3d(np.absolute(Ez),"Ez")
 # plot(np.absolute(Ez[:,:,N//2]),"Ez")
 print("process time: %s seconds ---" % (time.time() - start_time))
-    
