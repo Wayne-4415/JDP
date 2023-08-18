b652584aab9c5b7b6640aa26cb1c125cbb5da1b5
BakeSausage <joseph369351@gmail.com>
2023-08-18 16:25:05 +0800

    08181625

--- kenji.py
+++ kenji.py
@@ -2,15 +2,16 @@ import numpy as np
 from math import pi, sqrt, exp
 import cmath
 import matplotlib.pyplot as plt
+import time 
 
 ####################################
 '''
 define physic parameters and commonly used functions.
 
 '''
-N=7
+N=8
 Lx = 1E-6
-Lfx = 2**4/Lx
+Lfx = 4
 x = np.zeros((2**N))
 fx = np.zeros((2**N))
 
@@ -27,20 +28,14 @@ def plot(figure,title):
     # fig.colorbar(contourf)
     plt.show()
 
-xx, yy = np.meshgrid(x, x)
-fxx, fyy = np.meshgrid(fx, fx)
-def ft(f,x,fx):
-    temp = np.zeros((len(fx), len(fx)), dtype=complex)
-    for i in range(len(fx)):
-        for j in range(len(fx)):
-            temp[j,i] = np.sum(f*np.exp(-1j*2*pi*(xx*fx[i]+yy*fx[j])))
+def paddle(M,n):
+    temp = np.zeros((n, n), dtype=complex)
+    temp[n//2-len(M)//2:n//2+len(M)//2, n//2-len(M)//2:n//2+len(M)//2] = M
     return temp
 
-def ift(f,x,fx):
-    temp = np.zeros((len(x), len(x)), dtype=complex)
-    for i in range(len(x)):
-        for j in range(len(x)):
-            temp[j,i] = np.sum(f*np.exp(1j*2*pi*(fxx*x[i]+fyy*x[j])))
+def capture(M,n):
+    temp = np.zeros((n, n), dtype=complex)
+    temp = M[len(M)//2-n//2:len(M)//2+n//2, len(M)//2-n//2:len(M)//2+n//2]
     return temp
 
 def P_shift(x_shift,y_shift):
@@ -61,7 +56,7 @@ for i in range(2**N):
         if (abs(fx[i])-0.6*(0.25*Lfx))**2+(abs(fx[j])-0.5*(0.25*Lfx))**2 < 0.017*(0.25*Lfx)**2:
             S[i,j] = 1
 
-        
+
 P = np.zeros((2**N,2**N))
 for i in range(2**N):
     for j in range(2**N):
@@ -102,7 +97,7 @@ I = np.zeros((2**N,2**N))
 main program.
 
 '''
-
+start_time = time.time()
 s=0
 for i in range(2**N):
     for j in range(2**N):
@@ -110,33 +105,42 @@ for i in range(2**N):
             P_operator[s:s+1,:] = np.transpose(P_shift(-i+2**(N-1),-j+2**(N-1))).reshape(1,2**(2*N))
             # P_operator[s:s+1,:] = P_shift(-i+2**(N-1),-j+2**(N-1)).reshape(1,2**(2*N))
             s+=1
+print("calculate p_orerator time: %s seconds ---\n" % (time.time() - start_time))
+start_time = time.time()
 
 U, lamb, Vh = np.linalg.svd(P_operator, full_matrices=False)
 lamb = lamb/max(lamb)
 print("first 12 square of singular values are \n" + str(np.square(lamb[0:12])) + "\n")
-print("threshold = " + str(th) +", calculate " + str(sum(a > th for a in lamb)) + " eigenvalues")
-   
-ift_a = ift(a, x, fx)
+print("threshold = " + str(th) +", calculate " + str(sum(a > th for a in lamb)) + " eigenvalues\n")
+print("calculate SVD time: %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+
+ift_a = capture(np.fft.ifftshift(np.fft.ifft2(paddle(a, 2**(2*N-4)))), 2**N)
+print("calculate ft of mask time: %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
 
 for i in range(sum(a > th for a in lamb)):
     eig[i,:,:] = Vh[i,:].reshape(2**N,2**N)
-    eig_I[i,:,:] = ft(ift_a * eig[i,:,:], x, fx)
+    eig_I[i,:,:] = capture(np.fft.fft2(paddle(ift_a * eig[i,:,:], 2**(2*N-4))), 2**N)
     I += lamb[i]**2 * np.square(np.absolute(eig_I[i,:,:]))
 
 ####################################
 '''
 plot.
-'''
 
+'''
 # plot(P_operator,"P_operator")
 plot(S,"S")
 # plot(P,"P")
 plot(a,"a")
-# plot(np.real(ift_a),"ift_a")
-# plot(eig[0,:,:],"phi0")
-# plot(eig[1,:,:],"phi1")
-# plot(eig[2,:,:],"phi2")
-# plot(np.real(eig_I[0,:,:]),"eig_I0")
-# plot(np.imag(eig_I[1,:,:]),"eig_I1")
-# plot(np.imag(eig_I[2,:,:]),"eig_I2")
+plot(np.absolute(ift_a),"ift_a")
+plot(eig[0,:,:],"phi0")
+plot(eig[1,:,:],"phi1")
+plot(eig[2,:,:],"phi2")
+plot(np.absolute(eig_I[0,:,:]),"eig_I0")
+plot(np.absolute(eig_I[1,:,:]),"eig_I1")
+plot(np.absolute(eig_I[2,:,:]),"eig_I2")
 plot(I,"I")
+
+####################################
+print("calculate aerial image time: %s seconds ---" % (time.time() - start_time))
